751cac60291cb432c26f05452c5a5f73aa374dd7
Mark Mansour <mark@stateofflux.com>
2023-04-25 16:30:32 -0700

    Knight Logic

--- src/main/java/com/stateofflux/chess/model/KnightMoves.java
+++ src/main/java/com/stateofflux/chess/model/KnightMoves.java
@@ -1,22 +1,46 @@
 package com.stateofflux.chess.model;
 
-public class KnightMoves extends StraightLineMoves {
+public class KnightMoves extends BoardMoves {
+    int[] paths;
+
     protected KnightMoves(Board board, int location) {
         super(board, location);
-        // this is wrong
-        setupPaths();
     }
 
     protected void setupPaths() {
-        this.directions = new Direction[] {
-                Direction.UP_LEFT,
-                Direction.UP_RIGHT,
-                Direction.DOWN_LEFT,
-                Direction.DOWN_RIGHT
-        };
+        // no-op
     }
 
     @Override
-    public void findCaptureAndNonCaptureMovesInStraightLines() {
+    void findCaptureAndNonCaptureMoves() {
+        int rank = this.location / 8 + 1;  // one offset
+        int file = this.location % 8;      // zero offset
+
+        if (rank <= 7 && file >= 2) { checkKnightMove(6); } // 10 oclock
+        if (rank <= 6 && file >= 1) { checkKnightMove(15); } // 11 oclock
+
+        if (rank <= 6 && file <= 6) { checkKnightMove(17); } // 1 oclock
+        if (rank <= 7 && file <= 5) { checkKnightMove(10); } // 2 oclock
+
+        if (rank >= 2 && file <= 5) { checkKnightMove(-6); } // 4 oclock
+        if (rank >= 3 && file <= 6) { checkKnightMove(-15); } // 5 oclock
+
+        if (rank >= 3 && file >= 1) { checkKnightMove(-17); } // 7 oclock
+        if (rank >= 2 && file >= 2) { checkKnightMove(-10); } // 8 oclock
+    }
+
+    private void checkKnightMove(int offset) {
+        int nextPosition;
+        long nextPositionBit;
+
+        nextPosition = this.location + offset;
+        nextPositionBit = 1L << nextPosition;
+
+        if ((this.occupiedBoard & nextPositionBit) == 0)
+            this.nonCaptureMoves |= nextPositionBit;
+
+        if ((this.opponentBoard & nextPositionBit) != 0) {
+            this.captureMoves |= nextPositionBit;
+        }
     }
 }
