230b2fd92e496a5b8eab2c6a99bda4d7a48f0679
Mark Mansour <mark@stateofflux.com>
2023-12-28 16:59:58 -0800

    Knights use precalculated moves for improved performance.

--- src/main/java/com/stateofflux/chess/model/pieces/KnightMoves.java
+++ src/main/java/com/stateofflux/chess/model/pieces/KnightMoves.java
@@ -3,6 +3,30 @@ package com.stateofflux.chess.model.pieces;
 import com.stateofflux.chess.model.Board;
 
 public class KnightMoves extends PieceMoves {
+    static long[] KNIGHT_MOVES = new long[64];
+    static {
+        int rank;
+        int file;
+        long bb;
+
+        for(int i = 0; i < 64; i++) {
+            bb = 0L;
+            rank = Board.rank(i) + 1;  // one offset
+            file = Board.file(i);      // zero offset
+
+            if (rank <= 7 && file >= 2) { bb |= 1L << (i +  6); } // 10 oclock
+            if (rank <= 6 && file >= 1) { bb |= 1L << (i + 15); } // 11 oclock
+            if (rank <= 6 && file <= 6) { bb |= 1L << (i + 17); } // 1 oclock
+            if (rank <= 7 && file <= 5) { bb |= 1L << (i + 10); } // 2 oclock
+            if (rank >= 2 && file <= 5) { bb |= 1L << (i + -6); } // 4 oclock
+            if (rank >= 3 && file <= 6) { bb |= 1L << (i + -15); } // 5 oclock
+            if (rank >= 3 && file >= 1) { bb |= 1L << (i + -17); } // 7 oclock
+            if (rank >= 2 && file >= 2) { bb |= 1L << (i + -10); } // 8 oclock
+
+            KNIGHT_MOVES[i] = bb;
+        }
+    }
+
     int[] paths;
 
     public KnightMoves(Board board, int location) {
@@ -15,34 +39,7 @@ public class KnightMoves extends PieceMoves {
 
     @Override
     void findCaptureAndNonCaptureMoves() {
-        int rank = Board.rank(this.location) + 1;  // one offset
-        int file = Board.file(this.location);      // zero offset
-
-        if (rank <= 7 && file >= 2) { checkKnightMove(6); } // 10 oclock
-        if (rank <= 6 && file >= 1) { checkKnightMove(15); } // 11 oclock
-
-        if (rank <= 6 && file <= 6) { checkKnightMove(17); } // 1 oclock
-        if (rank <= 7 && file <= 5) { checkKnightMove(10); } // 2 oclock
-
-        if (rank >= 2 && file <= 5) { checkKnightMove(-6); } // 4 oclock
-        if (rank >= 3 && file <= 6) { checkKnightMove(-15); } // 5 oclock
-
-        if (rank >= 3 && file >= 1) { checkKnightMove(-17); } // 7 oclock
-        if (rank >= 2 && file >= 2) { checkKnightMove(-10); } // 8 oclock
-    }
-
-    private void checkKnightMove(int offset) {
-        int nextPosition;
-        long nextPositionBit;
-
-        nextPosition = this.location + offset;
-        nextPositionBit = 1L << nextPosition;
-
-        if ((this.occupiedBoard & nextPositionBit) == 0)
-            this.nonCaptureMoves |= nextPositionBit;
-
-        if ((this.opponentBoard & nextPositionBit) != 0) {
-            this.captureMoves |= nextPositionBit;
-        }
+        this.nonCaptureMoves |= KNIGHT_MOVES[this.location] & ~this.occupiedBoard;
+        this.captureMoves |= KNIGHT_MOVES[this.location] & this.opponentBoard;
     }
 }
