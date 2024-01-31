860817a3afb82321c1ab3a3c5515275d0eab1fc1
Mark Mansour <mark@stateofflux.com>
2024-01-30 17:33:38 -0800

    Human Player takes input from the console.

--- src/main/java/com/stateofflux/chess/model/player/HumanPlayer.java
+++ src/main/java/com/stateofflux/chess/model/player/HumanPlayer.java
@@ -2,16 +2,41 @@ package com.stateofflux.chess.model.player;
 
 import com.stateofflux.chess.model.Game;
 import com.stateofflux.chess.model.Move;
+import com.stateofflux.chess.model.MoveList;
 import com.stateofflux.chess.model.PlayerColor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
 
 public class HumanPlayer extends Player {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HumanPlayer.class);
 
     public HumanPlayer(PlayerColor pc) {
         super(pc);
     }
 
     public Move getNextMove(Game game) {
-        return game.generateMoves().get(0);
+        MoveList<Move> moves = game.generateMoves();
+        for(int i = 0; i < moves.size(); i++) {
+            LOGGER.info("{}. {} {}", i , moves.get(i).toLongSan(), moves.get(i).getPiece().getAlgebraicChar());
+        }
+
+        LOGGER.info("Which move number to play?");
+        Scanner scanner = new Scanner(System.in);  // in IntelliJ - Help / Edit Custom VM options.  Add line: -Deditable.java.test.console=true
+        int firstNum;
+
+        try {
+            firstNum = Integer.parseInt(scanner.nextLine());
+
+            if(firstNum > moves.size() || firstNum < 1)
+                firstNum = 1;
+        } catch(NumberFormatException nfe) {
+            firstNum = 1;
+        }
+
+        scanner.close();
+        return moves.get(firstNum - 1);
     }
 
     @Override
