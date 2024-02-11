ec4a8e9b425326f597312cfe57bd9bd02f712ea3
Mark Mansour <mark@stateofflux.com>
2024-02-11 11:40:05 -0800

    Added very simple evaluator that only examines material.  I'll be using this for debugging.

---
+++ src/main/java/com/stateofflux/chess/model/player/MaterialEvaluator.java
@@ -0,0 +1,47 @@
+package com.stateofflux.chess.model.player;
+
+import com.stateofflux.chess.model.*;
+import com.stateofflux.chess.model.pieces.Piece;
+
+import static com.stateofflux.chess.model.pieces.KingMoves.MATE_VALUE;
+import static java.lang.Long.bitCount;
+
+public class MaterialEvaluator implements Evaluator {
+
+    protected boolean endGame = false;
+
+    public MaterialEvaluator() {
+    }
+
+    @Override
+    public int evaluate(Game game, int depth) {
+        Board b = game.getBoard();
+
+        // sideMoved is the value of the move just completed.  The game counter has already moved
+        // on, so we need to reverse the player color.  Therefore, if the game thinks it is white's turn
+        // then it was black that just moved.
+        int sideMoved = game.getActivePlayerColor().isBlack() ? 1 : -1;
+
+        // from white's perspective.
+        if(game.isCheckmated()) {
+            // LOGGER.info("**************** CHECKMATED: {}", evaluatingMoves);
+            return (MATE_VALUE - depth) * sideMoved;
+        }
+
+        // from the perspective of the white player
+        int materialScore =
+            Evaluator.KING_VALUE * (bitCount(b.getWhiteKingBoard()) - bitCount(b.getBlackKingBoard()))
+                + Evaluator.QUEEN_VALUE * (bitCount(b.getWhiteQueenBoard()) - bitCount(b.getBlackQueenBoard()))
+                + Evaluator.ROOK_VALUE * (bitCount(b.getWhiteRookBoard()) - bitCount(b.getBlackRookBoard()))
+                + Evaluator.BISHOP_VALUE * (bitCount(b.getWhiteBishopBoard()) - bitCount(b.getBlackBishopBoard()))
+                + Evaluator.KNIGHT_VALUE * (bitCount(b.getWhiteKnightBoard()) - bitCount(b.getBlackKnightBoard()))
+                + Evaluator.PAWN_VALUE * (bitCount(b.getWhitePawnBoard()) - bitCount(b.getBlackPawnBoard()));
+
+        return (materialScore);
+    }
+
+    @Override
+    public int getNodesEvaluated() {
+        return 0;
+    }
+}
