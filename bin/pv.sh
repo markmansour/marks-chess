5c9863fc24dd88017b946b40d2a86b13b01bffa1
Mark Mansour <mark@stateofflux.com>
2024-03-08 15:39:49 -0800

    improved execution of ruby files

--- bin/pv.sh
+++ bin/pv.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+MYSELF="$(readlink -f "$0")"
+MYDIR="${MYSELF%/*}"
+
 for filename in ./log/*.xml; do
     [ -e "$filename" ] || continue
-    PV=$(ruby ./src/main/ruby/xpath-to-leaf.rb $filename)
+    PV=$(ruby $MYDIR/xpath-to-leaf.rb $filename)
     echo "$filename: ${PV}"
 done
 
