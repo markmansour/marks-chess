c3e75ed168e630fc0b868796292bfa958fc8fc73
Mark Mansour <mark@stateofflux.com>
2024-03-18 20:49:44 -0700

    Stop using Maven to start the program.

--- bin/app-ab-wd4-bd2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# To package run
-# $ mvn package assembly:single
-# -Xms<size>        set initial Java heap size
-# -Xmx<size>        set maximum Java heap size
-# -Xss<size>        set java thread stack size
-
-bash -c "cd /Users/markmansour/IdeaProjects/marks-chess && MAVEN_OPTS=\"-Xms1024m -Xmx2048m\" mvn exec:java -Dexec.mainClass=com.stateofflux.chess.App -Dexec.args=\"-wd 4 -bd 2\""
--- bin/app.sh
+++ bin/app.sh
@@ -2,4 +2,4 @@
 
 # To package run
 # $ mvn package assembly:single
-bash -c "cd /Users/markmansour/IdeaProjects/marks-chess && mvn exec:java -Dexec.mainClass=com.stateofflux.chess.App -Dexec.args=\"$(echo $@)\""
+bash -c "cd /Users/markmansour/IdeaProjects/marks-chess && java -jar target/markschess-1.0-SNAPSHOT-jar-with-dependencies.jar $(echo $@)"
