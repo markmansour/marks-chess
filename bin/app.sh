3a906d88464336b444af872d0d3f2613f3532e54
Mark Mansour <mark@stateofflux.com>
2024-03-22 14:47:17 -0700

    Increase robustness of app launcher.

--- bin/app.sh
+++ bin/app.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Define the file path
+script_dir=$(dirname "$0")
+chess_jar="$script_dir/../target/markschess-1.0-SNAPSHOT-jar-with-dependencies.jar"
+chess_jar=$(realpath "$chess_jar")
+
+# Check if the file does not exist
+if [ ! -f "$chess_jar" ]; then
+    # Print an error message to stderr
+    echo "Error: File '$chess_jar' not found." >&2
+    # Exit the script with a non-zero exit status
+    exit 1
+fi
+
 # To package run
 # $ mvn package assembly:single
-bash -c "cd /Users/markmansour/IdeaProjects/marks-chess && java -jar target/markschess-1.0-SNAPSHOT-jar-with-dependencies.jar $(echo $@)"
+bash -c "java -jar $chess_jar $(echo $@)"
